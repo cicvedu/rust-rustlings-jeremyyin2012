ef4fcd4af1d51e804435800ff06834244f452d35
Jeremy Yin <jeremyyin2012@gmail.com>
2023-08-07 14:45:55 +0800

    done: drive2 mutable raw pointer to a value of struct Foo instance in a Box

--- exercises/drive/drive2.rs
+++ exercises/drive/drive2.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint drive1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 
 struct Foo {
@@ -15,6 +14,11 @@ fn raw_pointer_to_box(address: usize) -> Box<Foo> {
     // address is a pointer that points to heap.
     // construct Box from this address, and modify Foo's b field to 
     // the string "hello"
+    let ptr = address as *mut Foo;
+    unsafe {
+        (*ptr).b = Some("hello".to_string());
+        Box::from_raw(ptr)
+    }
 }
 
 
